de0296ea1a7c6442d091ba16834ba4883f9b9812
Marco Bruno <marco.bruno.br@gmail.com>
2017-03-26 07:35:39 -0300

    Changed watson's conversion

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const watson = require('watson-developer-cloud')
 const tokenTelegram = '373446033:AAHLA1PbhkWGAKo0Y_IZoL4JtOUyd2Mq-4I'
 const bot = new TelegramBot(tokenTelegram, {polling: true})
 
-
-
 bot.on('message', (msg) => {
   let chatId = msg.chat.id
   let userCommand = msg.text
+  
   let conversation = watson.conversation({
     username: '546209fe-1bec-4870-9886-b60f1163efb2',
     password: '2Kxc7dYsjfHr',
