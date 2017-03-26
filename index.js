e4f4b56b6a9b37e6960f6732040eafc20c9cdf48
Marco Bruno <marco.bruno.br@gmail.com>
2017-03-26 07:41:19 -0300

    Created fallback to undefined

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new TelegramBot(tokenTelegram, {polling: true})
 bot.on('message', (msg) => {
   let chatId = msg.chat.id
   let userCommand = msg.text
-  
+
   let conversation = watson.conversation({
     username: '546209fe-1bec-4870-9886-b60f1163efb2',
     password: '2Kxc7dYsjfHr',
@@ -25,7 +25,7 @@ bot.on('message', (msg) => {
       console.log(msg.from.username)
       console.log(response.output.text[0])
       console.log('============================================================================================================================');
-      bot.sendMessage(chatId, response.output.text[0])
+      bot.sendMessage(chatId, response.output.text[0] || 'Essa eu não sei responder')
     }
   });
 })
